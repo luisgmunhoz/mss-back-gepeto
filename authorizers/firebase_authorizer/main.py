fde5e2982598c58bdcf0a8a8f8072cd1bade5206
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2024-09-20 19:09:39 -0300

    fix: lambda firebase authorizer

--- authorizers/firebase_authorizer/main.py
+++ authorizers/firebase_authorizer/main.py
@@ -1,22 +1,50 @@
+import json
 from typing import Literal, Optional
 from firebase_admin import auth, credentials, initialize_app
+import boto3
+from botocore.exceptions import ClientError
+import logging
 
-cred = credentials.Certificate("./serviceAccountKey.json")
+
+def get_secret():
+    secret_name = "prod/app/firebase_svc_acc_key"
+    region_name = "sa-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    return json.loads(get_secret_value_response["SecretString"])
+
+
+cred = credentials.Certificate(get_secret())
+
+
 initialize_app(cred)
 
 
 def lambda_handler(event, context):
     authorization = event["headers"].get("Authorization", "")
+    logging.info("splitting token")
     authorization_token = authorization.split("Bearer ")
     if len(authorization_token) < 2:
         policy = generate_policy("deny")
+        logging.info("invalid token")
         return policy
     token = authorization_token[-1]
+    logging.info("verifying token")
     try:
         decoded_token = auth.verify_id_token(token)
     except Exception:
         return generate_policy("deny")
-
+    logging.info("token verified")
     uid = decoded_token["uid"]
 
     return generate_policy("allow", uid)
