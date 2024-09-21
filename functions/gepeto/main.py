62a409df6cf21490c598fadcf632e55a2589bfcb
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2024-09-21 15:35:39 -0300

    feat: getting user data and returning initially to then make the request to chatgpt

--- functions/gepeto/main.py
+++ functions/gepeto/main.py
@@ -1,10 +1,21 @@
 import json
 from dataclasses import dataclass
+import os
+from firebase_admin import credentials, initialize_app, firestore
+import sm_utils
+
+# Retrieve the secret from Secrets Manager
+FIREBASE_SECRET_NAME = os.environ.get("FIREBASE_SECRET_NAME")
+FIREBASE_SECRET = sm_utils.get_secret(FIREBASE_SECRET_NAME)
+
+cred = credentials.Certificate(FIREBASE_SECRET)
+
+initialize_app(cred)
 
 
 @dataclass
 class Input:
-    pass
+    message: str
 
 
 @dataclass
@@ -14,9 +25,47 @@ class Output:
 
 def lambda_handler(event, context):
     print(event)
-    print(context)
+    uid = event.get("requestContext", {}).get("authorizer", {}).get("uid")
+    client = firestore.client()
+    initial_path = "users/" + uid
+    doc_ref = client.document(initial_path)
+    doc = doc_ref.get()
+    if not doc.exists:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"message": "User not found"}),
+            "headers": {"Access-Control-Allow-Origin": "*"},
+        }
+    user_data = doc.to_dict()
+    weight = user_data.get("weight")
+    height = user_data.get("height")
+    bmi = None
+    if weight is not None and height is not None:
+        bmi = weight / (height**2)
+    exams_col = doc_ref.collection("exams")
+    exams = exams_col.stream()
+    exams_data = [exam.to_dict() for exam in exams]
+    appointments_col = doc_ref.collection("appointments")
+    appointments = appointments_col.stream()
+    appointments_data = [appointment.to_dict() for appointment in appointments]
+    meds_col = doc_ref.collection("medications")
+    meds = meds_col.stream()
+    meds_data = [med.to_dict() for med in meds]
+    birthday = user_data.get("birthday")
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello World!"}),
+        "body": json.dumps(
+            {
+                "weight": weight,
+                "height": height,
+                "bmi": bmi,
+                "message": "Hello from Gepeto!",
+                "exams": exams_data,
+                "appointments": appointments_data,
+                "medications": meds_data,
+                "birthday": birthday,
+            }
+        ),
         "headers": {"Access-Control-Allow-Origin": "*"},
     }
