32ca42efcd459b0b8a48392f9e4c12a957b1727e
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2024-09-21 21:11:30 -0300

    feat: maybe openai analysis working

--- functions/gepeto/main.py
+++ functions/gepeto/main.py
@@ -1,5 +1,10 @@
+import os
+import openai
+from openai import OpenAI
+
 import json
 from dataclasses import dataclass
+import sm_utils
 
 
 @dataclass
@@ -33,6 +38,42 @@ def lambda_handler(event, context):
     appointments_data = body.get("appointments", [])
     meds_data = body.get("medications", [])
 
+    medical_data_str = json.dumps(
+        {
+            "weight": weight,
+            "height": height,
+            "bmi": bmi,
+            "message": message,
+            "exams": exams_data,
+            "appointments": appointments_data,
+            "medications": meds_data,
+            "birthday": birthday,
+        }
+    )
+    openai_secret_name = os.environ["OPENAI_SECRET_NAME"]
+    openai_secret = sm_utils.get_secret(openai_secret_name)
+    client = OpenAI(api_key=openai_secret)
+    prompt = f"The following data are, exams, bmi, weight, height, birthday, appointments booked and recurring meds the patient takes, do a analysis based on the users question:\n\n{medical_data_str}"
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {
+                "role": "user",
+                "content": message,
+            },
+        ],
+        max_tokens=1500,  # Adjust based on the amount of data and response size you expect
+        temperature=0.2,  # Lower temperature for more factual responses
+    )
+
+    # Get the text output from the response
+    analysis = response.choices[0].message.content
+
+    # Print or save the analysis
+    print("Analysis:")
+    print(analysis)
     return {
         "statusCode": 200,
         "body": json.dumps(
@@ -41,7 +82,7 @@ def lambda_handler(event, context):
                 "height": height,
                 "bmi": bmi,
                 "request": message,
-                "message": "Hello from Gepeto!",
+                "message": analysis,
                 "exams": exams_data,
                 "appointments": appointments_data,
                 "medications": meds_data,
